17da40c8a6a5e1a36839d74c8b37d64464a0f6d7
Vishal Verma <vishalverma.50103@gmail.com>
2025-05-12 19:42:35 +0530

    update theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
   if (isLoading) return <PageLoader />;
 
   return (
-    <div className="h-screen" data-theme={theme}>
+    <div className="min-h-screen" data-theme={theme}>
       <Routes>
         <Route
           path="/"
